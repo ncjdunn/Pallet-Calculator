1be53e24fb978173f4417516953347208dd4ed5b
ncjdunn <ncjdunn@gmail.com>
2025-04-18 10:56:07 -0400

    1st deplorr

---
+++ sw.js
@@ -0,0 +1,18 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('pallet-app').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'app-icon.png',
+        'logo.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
