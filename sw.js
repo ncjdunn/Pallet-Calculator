7fb7f0923cc10a9cd28f9fb7c3dfd614aa725722
ncjdunn <ncjdunn@gmail.com>
2025-04-19 00:07:23 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('pallet-app').then(cache =>
-    cache.addAll(['index.html','manifest.json','app-icon.png','logo.png'])));
+  e.waitUntil(
+    caches.open('pallet-app').then(cache =>
+      cache.addAll([
+        'index.html',
+        'manifest.json',
+        'app-icon.png',
+        'logo.png'
+      ])
+    )
+  );
 });
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
